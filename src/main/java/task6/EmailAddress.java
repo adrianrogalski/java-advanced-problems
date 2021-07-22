b938c876fabcc3dc2da6a61f1af2352e2a0289ed
Adrian <adrianrogalski99@gmail.com>
2021-07-23 00:04:52 +0200

    Zrobione zadanie 6. Klasy typu Value Object na podstawie adresu email, obsługa wyjątków

---
+++ src/main/java/task6/EmailAddress.java
@@ -0,0 +1,57 @@
+package task6;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class EmailAddress {
+    private final String email;
+
+    private EmailAddress(String email) {
+        this.email = email;
+    }
+
+    public static EmailAddress of(String email) {
+        if (hasOneAt(email)) {
+            if (isAlphanum(email) && hasDotAfterAt(email)) {
+                return new EmailAddress(email);
+            }
+        }
+        throw new IllegalEmailAddressException("Podano zły adres email");
+    }
+
+    private static boolean hasOneAt(String email) {
+        if (email.indexOf("@") != email.lastIndexOf("@")) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean hasDotAfterAt(String email) {
+        if (email.substring(email.indexOf("@")).indexOf(".") == -1) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isAlphanum(String email) {
+        email = StringUtils.remove(email, '@');
+        email = StringUtils.remove(email, '.');
+        System.out.println(email);
+        return StringUtils.isAlphanumeric(email);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String username() {
+        return email.substring(0,email.indexOf("@"));
+    }
+
+    public String host() {
+        return email.substring(email.indexOf("@") + 1);
+    }
+
+    public String topDomain() {
+        return email.substring(email.lastIndexOf("."));
+    }
+}
---
+++ src/main/java/task6/IllegalEmailAddressException.java
@@ -0,0 +1,7 @@
+package task6;
+
+public class IllegalEmailAddressException extends RuntimeException {
+    public IllegalEmailAddressException(String message) {
+        super(message);
+    }
+}
