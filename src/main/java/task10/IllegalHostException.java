a9f130b2c4b99ffa2537e75acd8f6773e22e77cc
Adrian <adrianrogalski99@gmail.com>
2021-07-24 09:26:14 +0200

    zrobiono część 1 zadania 10, osbługa wyjątków, tworzenie własnych wyjątków, zasady prawidłowej konstrukcji pomocniczych metod sprawdzających dla klasy value-object

---
+++ src/main/java/task10/IllegalHostException.java
@@ -0,0 +1,7 @@
+package task10;
+
+public class IllegalHostException extends RuntimeException {
+    public IllegalHostException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/task10/IllegalUsernameException.java
@@ -0,0 +1,7 @@
+package task10;
+
+public class IllegalUsernameException extends RuntimeException {
+    public IllegalUsernameException(String message) {
+        super(message);
+    }
+}
